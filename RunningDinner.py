f28ec2460d287d35d9dc06acdc0a27fdcf72b63f
SamAarts <505660@student.fontys.nl>
2023-09-14 14:34:07 +0200

    Opgeschoonde data met verzamelingen

--- RunningDinner.py
+++ RunningDinner.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 import math
 
-#Geef hier de nieuwe input op
-
+#Geef hier de naam van de nieuwe excel database op:
 ExcelInput = 'Running Dinner dataset 2022.xlsx'
-# read excel
+
+
+#Hier gaan we de excel tabbladen naar verschillende DataFrames inlezen.
 dfA = pd.read_excel(ExcelInput, sheet_name='Bewoners')
 dfB = pd.read_excel(ExcelInput, sheet_name='Adressen')
 dfC = pd.read_excel(ExcelInput, sheet_name='Paar blijft bij elkaar')
@@ -13,34 +14,36 @@ dfD = pd.read_excel(ExcelInput, skiprows=[0], sheet_name='Buren')
 dfE = pd.read_excel(ExcelInput, skiprows=[0], sheet_name='Kookte vorig jaar')
 dfF = pd.read_excel(ExcelInput, skiprows=[0], sheet_name='Tafelgenoot vorig jaar')
 
+#Hier gaan we de verschillende dataframes samenvoegen tot 1 enkele dataframe
+
 df = dfA.merge(dfB, how = 'left', on ='Huisadres')
 
 
-#Zorgen dat het huisadres hetzelfde is alsin personen. Dit om later makkelijker te kunnen slicen. 
+
+#Zorgen dat de lijst met huisadressen hetzelfde is als de lijst met personen. Dit om later makkelijker te kunnen slicen. String slicen
 for i in range(len(df['Huisadres'])):
     df.loc[i, 'Huisadres'] = df['Huisadres'][i][0:2] + '_' + df['Huisadres'][i][2:]
 
-df.to_excel('Output.xlsx')
 
+#Een lijst maken zodat er later een extra colom kan worden toegevoegd aan de dataframe om te zien of er mensen bij elkaar moeten blijven of niet. 
+Bijelkaarbinairlijst = list()
 for i in range(len(df['Huisadres'])):
-    if df['Huisadres'][i] in df['Bewoner'][i]:
-        	(df['Bewoner'][i])
-               
-Bewoners = df['Bewoner']
-print(Bewoners)
+    if df['Huisadres'][i] == 'WO_59' or df['Huisadres'][i] == 'WO_25':
+        Bijelkaarbinairlijst.append(1)
+    elif df['Huisadres'][i] != 'WO_59' and df['Huisadres'][i] != 'WO_25':
+        Bijelkaarbinairlijst.append(0)
+
+#De colom maken om te kunnen visualiseren welke personen er bij iemand moeten blijven
+
+df['Koppel blijft bijelkaar'] = Bijelkaarbinairlijst
+
+#Verzamelingen definieren
+Huisadres = df['Huisadres']
+Deelnemers = df['Bewoner']
+print(df)
+
+
+
+
+df.to_excel('Output.xlsx')  
          
-         
-         
-         
-         
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   print('hoi Sam')      
