7a5674c560dbc298f314ce082c6e6030fd55b75c
Annerosedegroot <433534@student.fontys.nl>
2023-09-14 11:55:49 +0200

    Merge op huisadres

--- RunningDinner.py
+++ RunningDinner.py
@@ -3,5 +3,13 @@ import numpy as np
 import math
 
 # read excel
-data = pd.read_excel('Running Dinner dataset 2022.xlsx')
-print(data.head())
+dfA= pd.read_excel('Running Dinner dataset 2022.xlsx', sheet_name='Bewoners')
+dfB = pd.read_excel('Running Dinner dataset 2022.xlsx', sheet_name='Adressen')
+dfC = pd.read_excel('Running Dinner dataset 2022.xlsx', sheet_name='Paar blijft bij elkaar')
+dfD = pd.read_excel('Running Dinner dataset 2022.xlsx', skiprows=[0], sheet_name='Buren')
+dfE = pd.read_excel('Running Dinner dataset 2022.xlsx', skiprows=[0], sheet_name='Kookte vorig jaar')
+dfF = pd.read_excel('Running Dinner dataset 2022.xlsx', skiprows=[0], sheet_name='Tafelgenoot vorig jaar')
+
+df = dfA.merge(dfB, how = 'left', on ='Huisadres')
+print(df)
+df.to_excel('Output.xlsx')
